2aebd83e3d8594c4e90a191627c4ccbf4e0b33c0
AbigailAcupan <157581891+AbigailAcupan@users.noreply.github.com>
2024-01-30 13:20:25 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once ('../config/config.php');
+require_once ('config/config.php');
 
 if (isset($_SESSION['isLoggedin'])) {
     header('location:login.php');
@@ -17,11 +17,12 @@ if (isset($_SESSION['isLoggedin'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body class="d-flex flex-column vh-100">
-    <?php include '../Header.php'; ?>
+    <?php include 'Header.php'; ?>
     <div class="container">
-        <h1>STUDENT
+        <h1>Welcome to Homepage 
+            kabankalan Catholic College
         </h1>
-        
+        <a href="add_student">
         <table>
             <thread>
                 <th>studentID</th><br><br>
@@ -37,10 +38,10 @@ if (isset($_SESSION['isLoggedin'])) {
         
         <?php
 
-    
+        
         ?>
     </div>
-    <?php include '../Footer.php'; ?>
+    <?php include 'Footer.php'; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"></script>
 
 </body> 
